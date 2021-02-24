25c6afc3913be3a94a89d3734673f0601e2c8390
Dean Netherton <dean.netherton@gmail.com>
2021-02-24 17:38:45 +1100

    Added custom logo for rc2014 dino build

---
+++ src/logo_rc2014.asm
@@ -0,0 +1,34 @@
+                include "systemvars.asm"
+                include "hardware.asm"
+
+                org     $8000
+;
+logo_ident:     db      "C-BIOS Logo ROM",$FF
+
+logo_show:
+	xor	a
+	ld	(0F3E9h),a	; Hide cursor
+	ld	(0F3EAh),a	; Back color = 0
+	ld	(0F3EBh),a	; Border color = 0
+
+	ld	a,2
+	ld	(0FCAFH),a
+	call	005Fh		; SCREEN 2 mode
+
+
+	ld	hl,image+7
+	ld	de,0
+	ld	bc,03800h
+	call	5Ch
+
+	; di
+	; halt
+
+	ret
+
+
+
+image:
+	incbin	"rc2014_logo.sc2"
+
+	ret
