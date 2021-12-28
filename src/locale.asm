83dc1d666f5dec7924c72a9e0392fdd966d8f952
Dean Netherton <dean.netherton@gmail.com>
2021-12-28 19:58:38 +1100

    startup message: removed vdu frequency

--- src/locale.asm
+++ src/locale.asm
@@ -9,24 +9,6 @@
                 ENDIF
         ENDIF
         db      $0D,$0A
-        IF VDP = TMS99X8
-                db      "Interrupt frequency: "
-        ELSE
-                db      "Default interrupt freq.: "
-        ENDIF
-        IF LOCALE_INT = LOCAL_INT_60HZ
-                db     "6"
-        ELSE
-                db     "5"
-        ENDIF
-        db      "0Hz"
-if INIT_80COLS
-	db 13, 10
-endif
-        ; the text fits exactly a line for non-MSX1 VDP
-        IF VDP = TMS99X8
-                db      $0D,$0A
-        ENDIF
         db      "Keyboard type: "
         IF LOCALE_KBD = LOCAL_KBD_US
                 db      "US"
